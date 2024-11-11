029f08816a9059c9a288b5f417b1f3448a8db274
saadAletek <said.elatik.602@gmail.com>
2024-11-11 11:14:34 +0300

    a

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,27 +5,33 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @ViewChild('themeSwitcher') themeSwitcher!: ElementRef;
 
   isLight = false;
+  isSwitching = false;
 
   toggleTheme() {
-    this.isLight =!this.isLight;
+    if (this.isSwitching) return;
+    this.isSwitching = true;
+    this.isLight = !this.isLight;
     this.themeSwitcher.nativeElement.classList.add('scale-125');
     setTimeout(() => {
-        this.themeSwitcher.nativeElement.classList.add('rotated');
+      this.themeSwitcher.nativeElement.classList.add('rotated');
       setTimeout(() => {
-        this.themeSwitcher.nativeElement.setAttribute('src', this.isLight ? 'images/dark.svg' : 'images/light.svg')
-      }, 400);
+        this.themeSwitcher.nativeElement.setAttribute(
+          'src',
+          this.isLight ? 'images/dark.svg' : 'images/light.svg'
+        );
+      }, 300);
       setTimeout(() => {
         this.themeSwitcher.nativeElement.classList.remove('rotated');
-    this.themeSwitcher.nativeElement.classList.remove('scale-125');
-
-      }, 600);
-    }, 200)
+        this.themeSwitcher.nativeElement.classList.remove('scale-125');
+        this.isSwitching = false;
+        //switch here
+      }, 400);
+    }, 100);
   }
-
 }
