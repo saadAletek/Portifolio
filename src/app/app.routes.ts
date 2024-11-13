72dd2f489c37d72d49672a08fbcac6e0b5d45dfb
saadAletek <said.elatik.602@gmail.com>
2024-11-13 13:48:52 +0300

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { MyworksComponent } from './myworks/myworks.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 export const routes: Routes = [
     {
         path : '',
         component : HomeComponent,
+    },
+    {
+        path : 'about-me',
+        component : AboutmeComponent,
+    },
+    {
+        path : 'contact-me',
+        component : ContactmeComponent,
+    },
+    {
+        path : 'my-works',
+        component : MyworksComponent,
+    },
+    {
+        path : 'blogs',
+        component : BlogsComponent,
     }
 ];
---
+++ src/app/myworks/myworks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myworks',
+  standalone: true,
+  imports: [],
+  templateUrl: './myworks.component.html',
+  styleUrl: './myworks.component.scss'
+})
+export class MyworksComponent {
+
+}
